9f72a7e990e4a181a1738b95e915a18f7b9a3b55
Andreas Kristiansen <andreas.kristiansen@lokalise.com>
2022-08-10 12:34:25 +0200

    Adding toArrayPreprocessor

---
+++ src/toArrayPreprocessor.ts
@@ -0,0 +1,17 @@
+const toArrayPreprocessor = (value: unknown) => {
+    if (Array.isArray(value)) {
+        return value
+    }
+
+    switch (typeof value) {
+        case 'string':
+        case 'number':
+        case 'boolean':
+            return [value]
+
+        default:
+            return value // could not coerce, return the original and face the consequences during validation
+    }
+}
+
+export default toArrayPreprocessor
