12fc2498278879da61832e510f52d0e9a9b907f5
Andreas Kristiansen <110380682+lokankr@users.noreply.github.com>
2022-08-10 10:18:18 +0200

    Feature/to string preprocessor (#2)
    * Increase coverage requirement to 100%
    
    * Add toStringPreprocessor
    
    * Update test/toStringPreprocessor.test.ts
    
    Co-authored-by: Igor Savin <iselwin@gmail.com>
    
    * Regex is an object, use function to increase coverage
    
    * Eslint fixes
    
    Co-authored-by: Igor Savin <iselwin@gmail.com>

---
+++ test/toStringPreprocessor.test.ts
@@ -0,0 +1,120 @@
+import z from 'zod'
+
+import toStringPreprocessor from '../src/toStringPreprocessor'
+
+describe('toStringPreprocessor', () => {
+    it('converts numbers to string', () => {
+        const CREATE_USER_SCHEMA = z.object({
+            age: z.preprocess(toStringPreprocessor, z.string().max(15)),
+            salary: z.preprocess(toStringPreprocessor, z.string()),
+            atomsCount: z.preprocess(toStringPreprocessor, z.string()),
+        })
+
+        const result = CREATE_USER_SCHEMA.parse({
+            age: 44,
+            salary: 12.34,
+            atomsCount: 35n,
+        })
+
+        expect(result).toEqual({
+            age: '44',
+            salary: '12.34',
+            atomsCount: '35',
+        })
+    })
+
+    it('converts booleans to string', () => {
+        const CREATE_USER_SCHEMA = z.object({
+            isAdmin: z.preprocess(toStringPreprocessor, z.string()),
+            isActive: z.preprocess(toStringPreprocessor, z.string()),
+        })
+
+        const result = CREATE_USER_SCHEMA.parse({
+            isAdmin: false,
+            isActive: true,
+        })
+
+        expect(result).toEqual({
+            isAdmin: 'false',
+            isActive: 'true',
+        })
+    })
+
+    it('converts dates to string', () => {
+        const CREATE_USER_SCHEMA = z.object({
+            createdAt: z.preprocess(toStringPreprocessor, z.string()),
+        })
+
+        const result = CREATE_USER_SCHEMA.parse({
+            createdAt: new Date('2022-01-01'),
+        })
+
+        expect(result).toEqual({
+            createdAt: '2022-01-01T00:00:00.000Z',
+        })
+    })
+
+    it('accepts string input', () => {
+        const CREATE_USER_SCHEMA = z.object({
+            name: z.preprocess(toStringPreprocessor, z.string()),
+        })
+
+        const result = CREATE_USER_SCHEMA.parse({
+            name: 'Batman',
+        })
+
+        expect(result).toEqual({
+            name: 'Batman',
+        })
+    })
+
+    it('accepts undefined input', () => {
+        const CREATE_USER_SCHEMA = z.object({
+            name: z.preprocess(toStringPreprocessor, z.optional(z.string())),
+        })
+
+        const result = CREATE_USER_SCHEMA.parse({
+            name: undefined,
+        })
+
+        expect(result).toEqual({
+            name: undefined,
+        })
+    })
+
+    it('fails when parsing unsupported types', () => {
+        const CREATE_USER_SCHEMA = z.object({
+            name: z.preprocess(toStringPreprocessor, z.optional(z.string())),
+        })
+
+        expect(() =>
+            CREATE_USER_SCHEMA.parse({
+                name: (x: string) => x,
+            }),
+        ).toThrow(/Expected string, received function/)
+    })
+
+    it('fails when parsing objects', () => {
+        const CREATE_USER_SCHEMA = z.object({
+            payload: z.preprocess(toStringPreprocessor, z.string()),
+        })
+
+        expect(() =>
+            CREATE_USER_SCHEMA.parse({
+                payload: { foo: 'bar' },
+            }),
+        ).toThrow(/Expected string, received object/)
+    })
+
+    it('fails when parsing null', () => {
+        const CREATE_USER_SCHEMA = z.object({
+            payload: z.preprocess(toStringPreprocessor, z.string()),
+        })
+
+        expect(() =>
+            CREATE_USER_SCHEMA.parse({
+                payload: null,
+            }),
+        ).toThrow(/Expected string, received null/)
+    })
+})
