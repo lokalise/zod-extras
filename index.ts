7925068add709eda1fc320f5109e973775e9f8b8
Andreas Kristiansen <andreas.kristiansen@lokalise.com>
2022-08-12 16:42:26 +0200

    BEF-43 Lint fixes

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export { default as toArrayPreprocessor } from "./src/toArrayPreprocessor"
-export { default as toBooleanPreprocessor } from "./src/toBooleanPreprocessor"
-export { default as toNumberPreprocessor } from "./src/toNumberPreprocessor"
-export { default as toStringPreprocessor } from "./src/toStringPreprocessor"
+export { default as toArrayPreprocessor } from './src/toArrayPreprocessor'
+export { default as toBooleanPreprocessor } from './src/toBooleanPreprocessor'
+export { default as toNumberPreprocessor } from './src/toNumberPreprocessor'
+export { default as toStringPreprocessor } from './src/toStringPreprocessor'
