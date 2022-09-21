23b9400a8ebe5e67b4540b51a09721a56dc3aea7
Andreas Kristiansen <110380682+lokankr@users.noreply.github.com>
2022-09-21 09:20:58 +0200

    Declare type=module in package.json (#27)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-unresolved */
-export { default as numberSplitFactory } from './dist/src/numberSplitFactory.js'
-export { default as stringSplitFactory } from './dist/src/stringSplitFactory.js'
-export { default as toArrayPreprocessor } from './dist/src/toArrayPreprocessor.js'
-export { default as toBooleanPreprocessor } from './dist/src/toBooleanPreprocessor.js'
-export { default as toNumberPreprocessor } from './dist/src/toNumberPreprocessor.js'
-export { default as toStringPreprocessor } from './dist/src/toStringPreprocessor.js'
+export { default as numberSplitFactory } from './dist/numberSplitFactory.js'
+export { default as stringSplitFactory } from './dist/stringSplitFactory.js'
+export { default as toArrayPreprocessor } from './dist/toArrayPreprocessor.js'
+export { default as toBooleanPreprocessor } from './dist/toBooleanPreprocessor.js'
+export { default as toNumberPreprocessor } from './dist/toNumberPreprocessor.js'
+export { default as toStringPreprocessor } from './dist/toStringPreprocessor.js'
